dad3236493765ddde34ef47920903b6df222dea2
Vladislav Tavrel <VladislavTavrel@coherentsolutions.com>
2025-03-26 22:17:04 +0100

    Refactor and add not completed testing

---
+++ UserService.Test/UserServiceTest.cs
@@ -0,0 +1,19 @@
+﻿namespace UserService.Test
+{
+    public class UserServiceTest : IClassFixture<UserServiceFixture>
+    {
+        private UserServiceFixture _fixture;
+
+        public UserServiceTest( UserServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        //To add *normal* tests I have to swap computers, so I'll do it later
+        [Theory]
+        [InlineData("","","", )]//how to pass DateTime in InlineData? че за дауны это делали
+        public void AddUser_InputIncorrectValues_ShouldReturnFalseBecauseOfValidation(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+
+        }
+    }
+}
